d650aeca2a2c3a5a3e342092ad8eb40796431f51
Tobias Bleckert <hola@tobiasbleckert.se>
2021-03-31 14:36:44 +0200

    Move back to cjs to avoid problems

--- dist/capitalize.js
+++ dist/capitalize.js
@@ -1,3 +1,10 @@
-export default function capitalize(str) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = capitalize;
+
+function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
